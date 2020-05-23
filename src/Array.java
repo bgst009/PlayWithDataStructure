496844904efeb20ee0afc76ecf2cb79c380a3020
bgst009 <1499135121@qq.com>
2020-05-23 17:55:55 +0800

    finish  chapter 2-4

--- src/Array.java
+++ src/Array.java
@@ -1,5 +1,7 @@
 import jdk.jfr.BooleanFlag;
 
+import java.util.Arrays;
+
 /**
  * @author yin_zhong_en
  * @version 1.0.0
@@ -12,7 +14,7 @@ public class Array {
     private int size;
 
     /**
-     * @param capacity
+     * @param capacity 容量
      */
     public Array(int capacity) {
         data = new int[capacity];
@@ -39,5 +41,79 @@ public class Array {
 
     public boolean isEmpty() {
         return size == 0;
+    }
+
+    /**
+     * 向数组中添加元素
+     *
+     * @param e element
+     * @throws IllegalAccessException 索引异常
+     */
+    public void addLast(int e) throws IllegalAccessException {
+        add(size, e);
+    }
+
+    /**
+     * 在所有元素前添加一个新的元素
+     *
+     * @param e element
+     */
+    public void addFirst(int e) throws IllegalAccessException {
+        add(0, e);
+    }
+
+    /**
+     * 在 index 位置插入 元素 e
+     *
+     * @param index 索引
+     * @param e     element
+     * @throws IllegalAccessException 索引异常
+     */
+    public void add(int index, int e) throws IllegalAccessException {
+        if (size == data.length) {
+            throw new IllegalAccessException("AddLast failed. Array is full");
+        }
+        if (index < 0 || index > size) {
+            throw new IllegalAccessException("AddLast failed. Array require index < 0 || index > size");
+        }
+        System.arraycopy(data, index, data, index + 1, size - index);
+        data[index] = e;
+        size++;
+    }
+
+    /**
+     * 获取 index 索引位置的元素
+     *
+     * @param index 索引
+     * @return data[index]
+     * @throws IllegalAccessException 索引异常
+     */
+    int get(int index) throws IllegalAccessException {
+        if (index < 0 || index >= size) {
+            throw new IllegalAccessException("Get failed, Index is illegal.");
+        }
+        return data[index];
+    }
+
+    /**
+     * 设置 index 索引位置的元素
+     *
+     * @param index 索引
+     * @throws IllegalAccessException 索引异常
+     */
+    void set(int index, int e) throws IllegalAccessException {
+        if (index < 0 || index >= size) {
+            throw new IllegalAccessException("Get failed, Index is illegal.");
+        }
+        data[index] = e;
+    }
+
+    @Override
+    public String toString() {
+        return "Array{" +
+                "data=" + Arrays.toString(data) +
+                ", size=" + size +
+                ", capacity=" + data.length +
+                '}';
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,14 @@
 public class Main {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IllegalAccessException {
 	// write your code here
+        Array array = new Array(20);
+        for (int i = 0; i < 10; i++) {
+            array.addLast(i);
+        }
+        System.out.println(array);
+        array.add(1,100);
+        array.addFirst(-1);
+        System.out.println(array);
     }
 }
