5b68aa4a111657a06b4a7d4c8e3a82ec87991884
bgst009 <1499135121@qq.com>
2020-05-23 14:16:00 +0800

    finish  chapter 2-9 lazy

--- src/Array.java
+++ src/Array.java
@@ -163,7 +163,7 @@ public class Array<E> {
         //自动回收 //loitering objects
         data[size] = null;
 
-        if (size == data.length / 2) {
+        if (size == data.length / 4 && data.length / 2 != 0) {
             resize(data.length / 2);
         }
 
