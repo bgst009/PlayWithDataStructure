6bcae517f35f3ece8c8a707ed05109d4ed3265cd
bgst009 <1499135121@qq.com>
2020-05-26 09:30:11 +0800

    finish  chapter 5-4 链表的天然递归结构性质，203号问题的递归解法

---
+++ 04-LinkedList/src/leetcode/Solution3.java
@@ -0,0 +1,36 @@
+package leetcode;
+
+/**
+ * @author yin_zhong_en
+ * @version 1.0.0
+ * @ClassName Solution3.java
+ * @createTime 2020年05月26日 09:11:00
+ */
+public class Solution3 {
+
+    public ListNode removeElements(ListNode head, int val) {
+        /**
+         * 最小问题的处理
+         */
+        if (head == null) {
+            return null;
+        }
+        /**
+         * 宏观问题的处理
+         */
+        head.next = removeElements(head.next, val);
+
+        return head.val == val ? head.next : head;
+    }
+
+
+    public static void main(String[] args) {
+
+        int[] nums = {1, 2, 6, 3, 4, 5, 6};
+        ListNode head = new ListNode(nums);
+        System.out.println(head);
+
+        ListNode res = (new Solution3()).removeElements(head, 6);
+        System.out.println(res);
+    }
+}
