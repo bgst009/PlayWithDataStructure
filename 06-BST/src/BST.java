d446dd3213c57149e180e3849563dc59208c3682
bgst009 <1499135121@qq.com>
2020-05-28 10:57:03 +0800

    finish  chapter 6-3 向二分搜索树中添加元素

--- 06-BST/src/BST.java
+++ 06-BST/src/BST.java
@@ -33,4 +33,52 @@ public class BST<E extends Comparable<E>> {
     public boolean isEmpty() {
         return size == 0;
     }
+
+    /**
+     * 向二分搜索树中添加新的元素e
+     *
+     * @param e 插入的元素
+     */
+    public void add(E e) {
+        if (root == null) {
+            root = new Node(e);
+            size++;
+        } else {
+            add(root, e);
+        }
+    }
+
+    /**
+     * 向以node为根的二分搜索树中插入元素e，递归算法
+     *
+     * @param node 根节点
+     * @param e    插入的元素
+     */
+    private void add(Node node, E e) {
+
+        /**
+         * 递归到底的情况
+         */
+        if (e.equals(node.e)) {
+            return;
+        } else if (e.compareTo(node.e) < 0 && node.left == null) {
+            node.left = new Node(e);
+            size++;
+            return;
+        } else if (e.compareTo(node.e) > 0 && node.right == null) {
+            node.right = new Node(e);
+            size++;
+            return;
+        }
+
+        /**
+         * 宏观处理流程
+         */
+        if (e.compareTo(node.e) < 0) {
+            add(node.left, e);
+        } else {
+            add(node.right, e);
+        }
+
+    }
 }
