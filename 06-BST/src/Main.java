0e942c4823c9241faa518eb5d25f5e48b68828eb
bgst009 <1499135121@qq.com>
2020-05-28 11:53:55 +0800

    finish  chapter 6-7 中序遍历，后序遍历

--- 06-BST/src/Main.java
+++ 06-BST/src/Main.java
@@ -11,9 +11,20 @@ public class Main {
         for (int num : nums) {
             bst.add(num);
         }
+        /////////////////
+        //      5      //
+        //    /   \    //
+        //   3    6    //
+        //  / \    \   //
+        // 2  4     8  //
+        /////////////////
         bst.preOrder();
         System.out.println();
 
-        System.out.println(bst);
+        bst.inOrder();
+        System.out.println();
+
+        bst.postOrder();
+        System.out.println();
     }
 }
