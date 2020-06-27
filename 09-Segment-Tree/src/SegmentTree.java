44e89e0b70a5811dd6087289485b18a89e2beeb3
bgst009 <1499135121@qq.com>
2020-06-27 11:45:22 +0800

    finish  chapter 9-3 线段树创建 (03-Building-Segment-Tree)

--- 09-Segment-Tree/src/SegmentTree.java
+++ 09-Segment-Tree/src/SegmentTree.java
@@ -1,8 +1,11 @@
 public class SegmentTree<E> {
   private E[] tree;
   private E[] data;
+  private Merger<E> merger;
 
-  public SegmentTree(E[] arr) {
+  public SegmentTree(E[] arr, Merger<E> merger) {
+
+    this.merger = merger;
 
     data = (E[]) new Object[arr.length];
     for (int i = 0; i < arr.length; i++) {
@@ -10,6 +13,32 @@ public class SegmentTree<E> {
     }
 
     tree = (E[]) new Object[4 * arr.length];
+    buildSegmentTree(0, 0, data.length - 1);
+  }
+
+  /**
+   * 在treeIndex的位置创建表示区间[l...r]的线段树
+   *
+   * @param treeIndex treeIndex的位置
+   * @param l left
+   * @param r right
+   */
+  private void buildSegmentTree(int treeIndex, int l, int r) {
+
+    // 递归到底
+    if (l == r) {
+      tree[treeIndex] = data[l];
+      return;
+    }
+
+    int leftTreeIndex = leftChild(treeIndex);
+    int rightTreeIndex = rightChild(treeIndex);
+
+    int mid = l + (r - l) / 2;
+    buildSegmentTree(leftTreeIndex, l, mid);
+    buildSegmentTree(rightTreeIndex, mid + 1, r);
+
+    tree[treeIndex] = merger.merger(tree[leftTreeIndex], tree[rightTreeIndex]);
   }
 
   /**
@@ -52,5 +81,24 @@ public class SegmentTree<E> {
    */
   private int rightChild(int index) {
     return 2 * index + 2;
+  }
+
+  @Override
+  public String toString() {
+    StringBuilder res = new StringBuilder();
+    res.append('[');
+    for (int i = 0; i < tree.length; i++) {
+      if (tree[i] != null) {
+        res.append(tree[i]);
+      } else {
+        res.append("null");
+      }
+
+      if (i != tree.length - 1) {
+        res.append(", ");
+      }
+    }
+    res.append(']');
+    return res.toString();
   }
 }
