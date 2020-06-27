0d78c0f0822062e74be30ce58153b06943c1d2c9
bgst009 <1499135121@qq.com>
2020-06-27 12:26:44 +0800

    finish  chapter 9-4 线段树中的区间查询 (04-Query-in-Segment-Tree)

--- 09-Segment-Tree/src/SegmentTree.java
+++ 09-Segment-Tree/src/SegmentTree.java
@@ -1,4 +1,5 @@
 public class SegmentTree<E> {
+
   private E[] tree;
   private E[] data;
   private Merger<E> merger;
@@ -13,7 +14,7 @@ public class SegmentTree<E> {
     }
 
     tree = (E[]) new Object[4 * arr.length];
-    buildSegmentTree(0, 0, data.length - 1);
+    buildSegmentTree(0, 0, arr.length - 1);
   }
 
   /**
@@ -25,7 +26,6 @@ public class SegmentTree<E> {
    */
   private void buildSegmentTree(int treeIndex, int l, int r) {
 
-    // 递归到底
     if (l == r) {
       tree[treeIndex] = data[l];
       return;
@@ -34,13 +34,13 @@ public class SegmentTree<E> {
     int leftTreeIndex = leftChild(treeIndex);
     int rightTreeIndex = rightChild(treeIndex);
 
+    // int mid = (l + r) / 2;
     int mid = l + (r - l) / 2;
     buildSegmentTree(leftTreeIndex, l, mid);
     buildSegmentTree(rightTreeIndex, mid + 1, r);
 
     tree[treeIndex] = merger.merger(tree[leftTreeIndex], tree[rightTreeIndex]);
   }
-
   /**
    * 获取数据量大小
    *
@@ -81,6 +81,58 @@ public class SegmentTree<E> {
    */
   private int rightChild(int index) {
     return 2 * index + 2;
+  }
+
+  /**
+   * 返回区间[queryL, queryR]的值
+   *
+   * @param queryL left
+   * @param queryR right
+   * @return value
+   */
+  public E query(int queryL, int queryR) {
+
+    if (queryL < 0
+        || queryL >= data.length
+        || queryR < 0
+        || queryR >= data.length
+        || queryL > queryR) {
+      throw new IllegalArgumentException("Index is illegal.");
+    }
+
+    return query(0, 0, data.length - 1, queryL, queryR);
+  }
+
+  /**
+   * 在以treeIndex为根的线段树中[l...r]的范围里，搜索区间[queryL...queryR]的值
+   *
+   * @param treeIndex treeIndex
+   * @param l left
+   * @param r right
+   * @param queryL query left
+   * @param queryR query right
+   * @return value
+   */
+  private E query(int treeIndex, int l, int r, int queryL, int queryR) {
+
+    if (l == queryL && r == queryR) {
+      return tree[treeIndex];
+    }
+
+    int mid = l + (r - l) / 2;
+    // treeIndex的节点分为[l...mid]和[mid+1...r]两部分
+
+    int leftTreeIndex = leftChild(treeIndex);
+    int rightTreeIndex = rightChild(treeIndex);
+    if (queryL >= mid + 1) {
+      return query(rightTreeIndex, mid + 1, r, queryL, queryR);
+    } else if (queryR <= mid) {
+      return query(leftTreeIndex, l, mid, queryL, queryR);
+    }
+
+    E leftResult = query(leftTreeIndex, l, mid, queryL, mid);
+    E rightResult = query(rightTreeIndex, mid + 1, r, mid + 1, queryR);
+    return merger.merger(leftResult, rightResult);
   }
 
   @Override
