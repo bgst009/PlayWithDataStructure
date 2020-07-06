041f57847d43f1bddacef091caf91657bb688516
bgst009 <1499135121@qq.com>
2020-07-06 18:45:04 +0800

    finish chapter 12-3 检查二分搜索树的性质和平衡性 (03-Checking-Balancing-and-Binary-Search-Property)

--- 12-AVL-Tree/src/AVLTree.java
+++ 12-AVL-Tree/src/AVLTree.java
@@ -30,6 +30,9 @@ public class AVLTree<K extends Comparable<K>, V> {
       System.out.println("Total different words: " + map.getSize());
       System.out.println("Frequency of PRIDE: " + map.get("pride"));
       System.out.println("Frequency of PREJUDICE: " + map.get("prejudice"));
+
+      System.out.println("is BST : " + map.isBST());
+      System.out.println("is Balanced : " + map.isBalanced());
     }
 
     System.out.println();
@@ -204,6 +207,59 @@ public class AVLTree<K extends Comparable<K>, V> {
       return 0;
     }
     return getHeight(node.left) - getHeight(node.right);
+  }
+
+  /**
+   * 判断该二叉树是否是一棵二分搜索树 。二分搜索树的中序遍历的结果为顺序
+   *
+   * @return boolean
+   */
+  public boolean isBST() {
+    ArrayList<K> keys = new ArrayList<>();
+    inOrder(root, keys);
+    for (int i = 1; i < keys.size(); i++) {
+      if (keys.get(i - 1).compareTo(keys.get(i)) > 0) {
+        return false;
+      }
+    }
+    return true;
+  }
+
+  private void inOrder(Node node, ArrayList<K> keys) {
+    if (node == null) {
+      return;
+    }
+
+    inOrder(node.left, keys);
+    keys.add(node.key);
+    inOrder(node.right, keys);
+  }
+
+  /**
+   * 判断该二叉树是否是一棵平衡二叉树
+   *
+   * @return boolean
+   */
+  public boolean isBalanced() {
+    return isBalanced(root);
+  }
+
+  /**
+   * 判断以Node为根的二叉树是否是一棵平衡二叉树，递归算法
+   *
+   * @param node Node为根的
+   * @return boolean
+   */
+  private boolean isBalanced(Node node) {
+    if (node == null) {
+      return true;
+    }
+
+    int balanceFactor = getBalanceFactor(node);
+    if (Math.abs(balanceFactor) > 1) {
+      return false;
+    }
+    return isBalanced(node.left) && isBalanced(node.right);
   }
 
   private class Node {
