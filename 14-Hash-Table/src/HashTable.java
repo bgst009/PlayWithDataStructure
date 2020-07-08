7fa868a8d1dab426f6795897f8885ce1f444ae32
bgst009 <1499135121@qq.com>
2020-07-08 16:38:21 +0800

    finish chapter 14-6 动态空间地址和复杂的分析  (06-Resizing-in-Hash-Table)

--- 14-Hash-Table/src/HashTable.java
+++ 14-Hash-Table/src/HashTable.java
@@ -1,6 +1,11 @@
 import java.util.TreeMap;
 
 public class HashTable<K, V> {
+
+  private static final int lowerTol = 2;
+  private static final int upperTol = 10;
+  private static final int initCapacity = 7;
+
   private TreeMap<K, V>[] hashtable;
   private int M;
   private int size;
@@ -34,6 +39,10 @@ public class HashTable<K, V> {
     } else {
       map.put(key, value);
       size++;
+
+      if (size >= upperTol * M) {
+        resize(2 * M);
+      }
     }
   }
 
@@ -43,6 +52,10 @@ public class HashTable<K, V> {
     if (map.containsKey(key)) {
       ret = map.remove(key);
       size--;
+
+      if (size <= lowerTol * M && M / 2 >= initCapacity) {
+        resize(M / 2);
+      }
     }
     return ret;
   }
@@ -61,5 +74,27 @@ public class HashTable<K, V> {
 
   public V get(K key) {
     return hashtable[hash(key)].get(key);
+  }
+
+  private void resize(int newM) {
+    TreeMap<K, V>[] newHastable = new TreeMap[newM];
+
+    for (int i = 0; i < newM; i++) {
+      newHastable[i] = new TreeMap<>();
+    }
+
+    /***********/
+    int oldM = M;
+    this.M = newM;
+    /***********/
+
+    for (int i = 0; i < oldM; i++) {
+      TreeMap<K, V> map = hashtable[i];
+      for (K key : map.keySet()) {
+        newHastable[hash(key)].put(key, map.get(key));
+      }
+    }
+
+    this.hashtable = newHastable;
   }
 }
