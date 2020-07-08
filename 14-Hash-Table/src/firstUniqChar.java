afd7538937c2586378d95b1677147eaa0c03aa59
bgst009 <1499135121@qq.com>
2020-07-08 09:21:04 +0800

    finish chapter 14-1 哈希表基础 (01-Hash-Table-Basics)

---
+++ 14-Hash-Table/src/firstUniqChar.java
@@ -0,0 +1,37 @@
+// 给定一个字符串，找到它的第一个不重复的字符，并返回它的索引。如果不存在，则返回 -1。
+//
+//
+//
+// 示例：
+//
+// s = "leetcode"
+// 返回 0
+//
+// s = "loveleetcode"
+// 返回 2
+//
+//
+//
+//
+// 提示：你可以假定该字符串只包含小写字母。
+// Related Topics 哈希表 字符串
+// 👍 226 👎 0
+
+// leetcode submit region begin(Prohibit modification and deletion)
+class Solution {
+  public int firstUniqChar(String s) {
+    int[] freq = new int[26];
+
+    for (int i = 0; i < s.length(); i++) {
+      freq[s.charAt(i) - 'a']++;
+    }
+
+    for (int i = 0; i < s.length(); i++) {
+      if (freq[s.charAt(i) - 'a'] == 1) {
+        return i;
+      }
+    }
+    return -1;
+  }
+}
+// leetcode submit region end(Prohibit modification and deletion)
